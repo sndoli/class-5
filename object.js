d9a623b197b3fa39abbb4a61edb87ebe70c05d33
sndoli <sabikunnahardoli@gmail.com>
2024-10-08 21:04:34 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,8 @@
+var objectName = {
+    image : "#" ,
+    name : "" ,
+    price : 5 
+};
+// console.log(objectName)
+
+console.log(objectName.name)
---
+++ sum.js
@@ -0,0 +1,4 @@
+var a = 5 ;
+var b = 10 ;
+var sum = a + b ;
+console.log(sum)
